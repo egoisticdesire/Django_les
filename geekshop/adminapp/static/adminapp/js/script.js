d0b1f8f2a2c0bb996f0faeb439016aa52d03177c
Andr <kfkfjqjq@mail.ru>
2022-02-09 15:19:23 +0400

    lesson_2_8

--- geekshop/adminapp/static/adminapp/js/script.js
+++ geekshop/adminapp/static/adminapp/js/script.js
@@ -2,41 +2,48 @@ const body = document.querySelector('body'),
     sidebar = body.querySelector('.sidebar'),
     toggle = body.querySelector('.toggle'),
     searchBtn = body.querySelector('.search-box'),
-    modeSwitch = body.querySelector('.toggle-switch'),
+    themeSwitcher = body.querySelector('.toggle-switch'),
     modeText = body.querySelector('.mode-text'),
     modeTableCard = body.querySelector('.card'),
     modeTableCardF = body.querySelector('.card-footer'),
     modeTableCardH = body.querySelector('.card-header'),
     logo = body.querySelector('ion-icon');
 
+searchBtn.addEventListener('click', () => {
+    sidebar.classList.remove('close');
+    localStorage.setItem('sidebar', sidebar.classList);
+});
 
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
+    localStorage.setItem('sidebar', sidebar.classList);
 });
 
-searchBtn.addEventListener('click', () => {
-    sidebar.classList.remove('close');
-});
-
-modeSwitch.addEventListener('click', () => {
+themeSwitcher.addEventListener('click', () => {
     body.classList.toggle('dark');
     logo.classList.toggle('dark');
+    localStorage.setItem('theme', body.classList);
+});
 
-    if (body.classList.contains('dark')) {
+window.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark');
         modeText.innerText = 'Light mode';
-    } else {
-        modeText.innerText = 'Dark mode';
-    }
-
-    if (logo.classList.contains('dark')) {
         logo.style.color = '#ccc';
         modeTableCard.style.border = '1px solid rgba(204, 204, 204, .125)';
         modeTableCardF.style.borderTop = '1px solid rgba(204, 204, 204, .05)';
         modeTableCardH.style.borderBottom = '1px solid rgba(204, 204, 204, .05)';
     } else {
+        body.classList.remove('dark');
+        modeText.innerText = 'Dark mode';
         logo.style.color = '#707070';
         modeTableCard.style.border = '1px solid rgba(112, 112, 112, .125)';
         modeTableCardF.style.borderTop = '1px solid rgba(112, 112, 112, .05)';
         modeTableCardH.style.borderBottom = '1px solid rgba(112, 112, 112, .05)';
+    }
+    if (localStorage.getItem('sidebar') === 'sidebar close') {
+        sidebar.classList.add('close');
+    } else {
+        sidebar.classList.remove('close');
     }
 });
